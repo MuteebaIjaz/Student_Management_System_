2bd7561a2fbdd8cab9c48235c428fb5c08a1d671
MuteebaIjaz <Muteebabaloch08@gmail.com>
2026-04-18 02:35:07 +0500

    Final database connection fix

--- includes/conn.php
+++ includes/conn.php
@@ -1,13 +1,14 @@
 <?php
 
-$conn = mysqli_connect("localhost","root","","student_management_system","3306");
-$db_host = getenv('DB_HOST') ?: "localhost";
-$db_user = getenv('DB_USER') ?: "root";
-$db_pass = getenv('DB_PASS') ?: "";
-$db_name = getenv('DB_NAME') ?: "student_management_system";
+$conn = mysqli_connect(
+    getenv('MYSQLHOST'),
+    getenv('MYSQLUSER'),
+    getenv('MYSQLPASSWORD'),
+    'railway',
+    getenv('MYSQLPORT')
+);
 
-$conn = mysqli_connect($db_host, $db_user, $db_pass, $db_name, "3306");
-if(!$conn){
-    die("DB Connection Failed".mysqli_connect_error());
+if (!$conn) {
+    die("DB Connection Failed: " . mysqli_connect_error());
 }
 ?>
